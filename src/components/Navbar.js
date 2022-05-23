ae51ba6180be43d86c9a84023cb00d7698384bac
Ramcas42 <ramsescastillo42@gmail.com>
2022-05-23 14:41:00 -0400

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import airbnbLogo from "../images/airbnb-logo.png";
+
+export default function Navbar() {
+    return (
+        <nav>        
+        
+        <img src={airbnbLogo} alt="airbnb logo" />
+        
+        </nav>
+
+    )
+}
